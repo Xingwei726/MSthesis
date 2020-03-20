4b9b74345a4905b017e86fa9e905e99a5601a436
EC2 Default User <ec2-user@ip-172-31-27-45.ec2.internal>
2020-03-20 00:43:08 +0000

    updates

--- web/scriptP1.js
+++ web/scriptP1.js
@@ -14,88 +14,95 @@
 //   }
 // }
 
-
-
+var countryName=[];
+var quantity=[];
+var year =[];
+var yearQuantity=[];
+         
+var margin = { top: 150, right: 50, bottom: 350, left: 50 },
+    width2 = 1440 - margin.left - margin.right,
+    height2 = 2000 - margin.top - margin.bottom;
 
 var width = 1220,
     height = 1000;
-        
+
+var size = 9;
+var groupSpacing = 3.5;
+var cellSpacing = 2;
+
+var xScale = d3.scaleBand()
+  .domain([0, 39])
+  .range([0, width2])
+//   .padding(0.1);
+  
+var yScale = d3.scaleLinear()
+  .domain([1500, 4900])
+  .range([0, height2-400]);
+
+// var x = d3.scaleBand()
+//           .range([0, width])
+//           .padding(0.1);
+// var y = d3.scaleLinear()
+//           .range([height, 0]);
+
+
+
 var svg = d3.select('#intro')
       .append('svg')
       .attr('width', width)
       .attr('height', height); 
 
 
-//square legend units      
-        const numPerRow = 10
-        const size = 10
-        const groupSpacing = 3.5;
-        const cellSpacing = 2;
-
-
-        const scale = d3.scaleLinear()
-          .domain([0, numPerRow -1])
-          .range([0, size * numPerRow]);
-          
+    //square legend units      
         svg.selectAll('rect')
           .data(d3.range(100))
           .enter()
           .append('rect')
           .attr('x', (d, i) => {
-            // const n = i % numPerRow
-            // return scale(n)
-          var x0 = Math.floor(i / 100) % 10, x1 = Math.floor(i % 10);
-          return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
+              var x0 = Math.floor(i / 100) % 10, x1 = Math.floor(i % 10);
+              return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
           })
           .attr('y', (d, i) => {
-            // const n = Math.floor(i / 10)
-            // return scale(n)
-          var y0 = Math.floor(i / 1000), y1 = Math.floor(i % 100 / 10);
-          return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10);
+              var y0 = Math.floor(i / 1000), y1 = Math.floor(i % 100 / 10);
+              return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10);
           }) 
           .attr('width', size)
           .attr('height', size)
-        //   .attr('fill', 'tomato')
           .attr('fill', '#FFFAF0')
           .attr('stroke-width', 2)
           .attr("transform", "translate(270, 480)");
+
+
+
+
+d3.csv("globalsupply2019.csv").then(function(data) {
+    for (var i=0; i<data.length; i++){
+        countryName.push(data[i].Country);
+        quantity.push(data[i].Tons);
+    }
+    console.log("CountryNames:",countryName);
+    console.log("Quantity:",quantity);
+
           
-//square legend text  
-// svg.append("text")
-//         .attr("x", -15)
-//         .attr("y", -10)
-//         .attr("text-anchor", "left")
-//         .style("font-size", "14px")
-//         .style("fill", "#FFFFFF")
-//         .text("Hour");
-        
-
-
-var margin = { top: 150, right: 100, bottom: 350, left: 40 },
-    width2 = 1440 - margin.left - margin.right,
-    height2 = 2000 - margin.top - margin.bottom;
-        
-var svg2 = d3.select('#graph1')
-      .append('svg')
-      .attr('width', width2)
-      .attr('height', height2)
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//Graph1
+        var svg2 = d3.select('#graph1')
+          .append('svg')
+          .attr('width', width2)
+          .attr('height', height2)
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+          
       
-var totalGraph = svg2.selectAll('rect')
-          .data(d3.range(4831))
+        var totalGraph = svg2.selectAll('rect').append("g")
+        var totalGraphGroup = totalGraph.data(d3.range(4831))
           .enter()
           .append('rect')
           .attr('x', (d, i) => {
-            // const n = i % numPerRow
-            // return scale(n)
-          var x0 = Math.floor(i / 100) % 10, x1 = Math.floor(i % 10);
-          return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
+              var x0 = Math.floor(i / 100) % 10, x1 = Math.floor(i % 10);
+              return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
           })
           .attr('y', (d, i) => {
-            // const n = Math.floor(i / 10)
-            // return scale(n)
-          var y0 = Math.floor(i / 1000), y1 = Math.floor(i % 100 / 10);
-          return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10)+800-20;
+              var y0 = Math.floor(i / 1000), y1 = Math.floor(i % 100 / 10);
+              return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10)+800;
           }) 
           .attr('width', size/2)
           .attr('height', size/2)
@@ -109,12 +116,180 @@ var totalGraph = svg2.selectAll('rect')
               d3.select(this)
               .style('opacity',1);
           });
-//transition of the squares
-totalGraph.transition()
+
+          
+        //transition of the squares
+        totalGraphGroup.transition()
           .attr('width', size)
           .attr('height', size)
+          .duration(500);
+
+
+        // var colorSquare = totalGraph.data(d3.range(100))
+        //           .enter()
+        //           .append('rect')
+        //           .attr('x', (d, i) => {
+        //               var x0 = Math.floor(i / 100) % 10, x1 = Math.floor(i % 10);
+        //               return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
+        //           })
+        //           .attr('y', (d, i) => {
+        //               var y0 = Math.floor(i / 1000), y1 = Math.floor(i % 100 / 10);
+        //               return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10)+800;
+        //           }) 
+        //           .attr('width', size)
+        //           .attr('height', size)
+        //           .attr('fill', 'tomato')
+        //           .attr('stroke-width', 2)
+
+
+        var tooltip = d3.select("#graph1")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("position", "absolute")
+            .style("color", "tomato")
+			.style("font-family", "gopher")
+			.style("font-size", "18px")
+            .style("padding", "8px")
+            .style("left", "1000px")
+            .style("top", "915px")
+
+        var mouseover = function(d) {
+            tooltip
+              .html("" + d.Ton + "Tons")
+              .style("opacity", 1);
+            d3.select(this)
+              .attr('fill', "tomato");
+            // colorSquare
+            //   .data(d3.range(1000))
+            //   .style("opacity", 1);
+              
+          }
+
+        var mouseleave = function(d) {
+            tooltip
+              .style("opacity", 0)
+            d3.select(this)
+              .attr('fill', "#FFFAF0");
+          }
+
+//Country Names Legend
+		  svg2.selectAll("text")
+			   .data(data)
+			   .enter()
+			   .append("text")
+			   .attr("x", 200)
+			   .attr("y", function(d,i) {
+			   		return 100 + i*15;
+			   })
+			   .attr("text-anchor", "left")
+			   .attr("font-family", "sans-serif")
+			   .style("font-size", "12px")
+			   .attr("fill", "#FFFAF0")
+			   .text(function(d) {
+			   		return d.Country;
+			   })
+			   .attr("transform", "translate(950, 710)")
+               .on('mouseover', mouseover)
+               .on('mouseleave', mouseleave)
+               
+               
+// Graph Title
+          svg2.append("text")
+			   .attr("x", 0)
+			   .attr("y", 200)
+			   .attr("text-anchor", "left")
+			   .attr("font-family", "gopher")
+			   .style("font-size", "36px")
+			   .attr("fill", "#FFFAF0")
+			   .text( "Global Cocoa Supply 2019")
+			   .attr("transform", "translate(0, 530)")
+
+
+//Total Count (static)
+          svg2.append("text")
+			   .attr("x", 0)
+			   .attr("y", 200)
+			   .attr("text-anchor", "left")
+			   .attr("font-family", "gopher")
+			   .style("font-size", "18px")
+			   .attr("fill", "#FFFAF0")
+			   .text( "Total Amount:")
+			   .attr("transform", "translate(0, 560)")
+
+          svg2.append("text")
+			   .attr("x", 0)
+			   .attr("y", 200)
+			   .attr("text-anchor", "left")
+			   .attr("font-family", "gopher")
+			   .style("font-size", "18px")
+			   .attr("fill", "#FFFAF0")
+			   .text( "4,831,885 Tons")
+			   .attr("transform", "translate(0, 585)")
+
+//Selected Count (change based on selection)
+          svg2.append("text")
+			   .attr("x", 0)
+			   .attr("y", 200)
+			   .attr("text-anchor", "left")
+			   .attr("font-family", "gopher")
+			   .style("font-size", "18px")
+			   .attr("fill", "tomato")
+			   .text( "Selected Amount:")
+			   .attr("transform", "translate(950, 560)")
+
+    console.log("CountryNames:",countryName);
+    console.log("Quantity:",quantity);
+
+ 
+});
+
+
+d3.csv("globalproductiontrend.csv").then(function(data) {
+    for (var i=0; i<data.length; i++){
+        year.push(data[i].Year);
+        yearQuantity.push(data[i].Tons);
+    }
+    console.log("Year:",year);
+    console.log("Year Quantity:",yearQuantity);
+
+
+//Graph2
+        var svg3 = d3.select('#graph2')
+          .append('svg')
+          .attr('width', width2)
+          .attr('height', height2)
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        svg3
+          .selectAll('.bar')
+          .data(data)
+          .enter()
+          .append('rect')
+          .attr("x", function(d,i) {
+			  return i*33;
+    	  }) 
+          .attr("width", 30)
+    	  .attr('y', function(d) { 
+    	      return height2 - yScale(d.Tons); 
+    	  })
+          .attr("height", function(d) {
+			  return yScale(d.Tons);
+    	  })
           .attr('fill', '#FFFAF0')
-        //   .delay(function(d,i) {
-        //       return i * 20;
-        //   })
-          .duration(500)
+          .attr('stroke-width', 2)
+          
+// Graph Title
+          svg3.append("text")
+			   .attr("x", 0)
+			   .attr("y", 0)
+			   .attr("text-anchor", "left")
+			   .attr("font-family", "gopher")
+			   .style("font-size", "36px")
+			   .attr("fill", "#FFFAF0")
+			   .text( "Global Cocoa Production")
+			   .attr("transform", "translate(0, 530)")
+
+
+ 
+});
