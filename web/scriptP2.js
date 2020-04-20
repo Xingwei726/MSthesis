8ffc0168d68aac6d8614ce2d3288ce44c168ecc3
EC2 Default User <ec2-user@ip-172-31-27-45.ec2.internal>
2020-04-20 03:23:50 +0000

    refinements

--- web/scriptP2.js
+++ web/scriptP2.js
@@ -2,16 +2,16 @@
 // var groupSpacing = 5;
 // var cellSpacing = 5;
 
-var width = 1440,
-    height = 700;
+// var width = 1440,
+//     height = 700;
 
-var margin = { top: 100, right: 50, bottom: 100, left: 50 },
-    width2 = 1440 - margin.left - margin.right,
-    height2 = 2000 - margin.top - margin.bottom;
+// var margin = { top: 100, right: 50, bottom: 100, left: 50 },
+//     width2 = 1440 - margin.left - margin.right,
+//     height2 = 2000 - margin.top - margin.bottom;
 
-var size = 9;
-var groupSpacing = 3.5;
-var cellSpacing = 2;
+// var size = 9;
+// var groupSpacing = 3.5;
+// var cellSpacing = 2;
     
 // //// 3.4 cocoa plantation methods (squares)
 // var svg5 = d3.select('#method')
@@ -95,118 +95,90 @@ $('.count').each(function () {
         
 // d3.csv("./data/forests.csv").then(function(data) {
 
-var data1 = [{ category: "chocolate", cacao: 500 }]
-var data2 = [{ category: "tree", cacao: 1200 }]
+// var data1 = [{ category: "chocolate", cacao: 500 }]
+// var data2 = [{ category: "tree", cacao: 1200 }]
 
-var svg = d3.select('#calculator')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .attr('background', '#000000')
-    .attr("transform", "translate(0," + margin.top + ")")
-    .attr("transform","translate(" + width/2 + "," + 50 + ")")
+// var svg = d3.select('#calculator')
+//     .append('svg')
+//     .attr('width', width)
+//     .attr('height', height)
+//     .attr('background', '#000000')
+//     .attr("transform", "translate(0," + margin.top + ")")
+//     .attr("transform","translate(" + width/2 + "," + 50 + ")")
 
 
-var chocolateCacao = 500
-var treeCacao = 1200
-var podCacao = 40
+// var chocolateCacao = 500
+// var treeCacao = 1200
+// var podCacao = 40
 
-        for (var z=0; z<chocolateCacao; z++){
-          var chocolate = svg.append('g').selectAll('rect')
-              .data(d3.range(chocolateCacao))
-              .enter()
-              .append('rect')
-              .attr('x', (d, i) => {
-                  var x0 = Math.floor(i / 100) % 5, x1 = Math.floor(i % 10);
-                  return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
-              })
-              .attr('y', (d, i) => {
-                  var y0 = Math.floor(i / 500), y1 = Math.floor(i % 100 / 10);
-                  return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10);
-              }) 
-              .attr('width', size)
-              .attr('height', size)
-              .attr('fill', '#000000')
-              .style('opacity',0)
-              .attr('stroke-width', 2)
-            //   .on("mouseover", mouseover)
-            //   .on("mouseleave",mouseleave)
-        }
+//         for (var z=0; z<chocolateCacao; z++){
+//           var chocolate = svg.append('g').selectAll('rect')
+//               .data(d3.range(chocolateCacao))
+//               .enter()
+//               .append('rect')
+//               .attr('x', (d, i) => {
+//                   var x0 = Math.floor(i / 100) % 5, x1 = Math.floor(i % 10);
+//                   return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
+//               })
+//               .attr('y', (d, i) => {
+//                   var y0 = Math.floor(i / 500), y1 = Math.floor(i % 100 / 10);
+//                   return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10);
+//               }) 
+//               .attr('width', size)
+//               .attr('height', size)
+//               .attr('fill', '#000000')
+//               .style('opacity',0)
+//               .attr('stroke-width', 2)
+//             //   .on("mouseover", mouseover)
+//             //   .on("mouseleave",mouseleave)
+//         }
         
         
-        var tree = svg.append('g')
-              .selectAll('rect')
-              .data(d3.range(treeCacao))
-              .enter()
-              .append('rect')
-              .attr('x', (d, i) => {
-                  var x0 = Math.floor(i / 100) % 5, x1 = Math.floor(i % 10);
-                  return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
-              })
-              .attr('y', (d, i) => {
-                  var y0 = Math.floor(i / 500), y1 = Math.floor(i % 100 / 10);
-                  return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10);
-              }) 
-              .attr('width', size)
-              .attr('height', size)
-              .attr('fill', '#000000')
-              .style('opacity',0)
+//         // var tree = svg.append('g')
+//         //       .selectAll('rect')
+//         //       .data(d3.range(treeCacao))
+//         //       .enter()
+//         //       .append('rect')
+//         //       .attr('x', (d, i) => {
+//         //           var x0 = Math.floor(i / 100) % 5, x1 = Math.floor(i % 10);
+//         //           return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
+//         //       })
+//         //       .attr('y', (d, i) => {
+//         //           var y0 = Math.floor(i / 500), y1 = Math.floor(i % 100 / 10);
+//         //           return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10);
+//         //       }) 
+//         //       .attr('width', size)
+//         //       .attr('height', size)
+//         //       .attr('fill', '#000000')
+//         //       .style('opacity',0)
 
 
-        var pod = svg.append('g').selectAll('rect')
-              .data(d3.range(podCacao))
-              .enter()
-              .append('rect')
-              .attr('x', (d, i) => {
-                  var x0 = Math.floor(i / 100) % 5, x1 = Math.floor(i % 10);
-                  return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
-              })
-              .attr('y', (d, i) => {
-                  var y0 = Math.floor(i / 500), y1 = Math.floor(i % 100 / 10);
-                  return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10);
-              }) 
-              .attr('width', size)
-              .attr('height', size)
-              .attr('fill', '#000000')
-              .style('opacity',0)        
+//         // var pod = svg.append('g').selectAll('rect')
+//         //       .data(d3.range(podCacao))
+//         //       .enter()
+//         //       .append('rect')
+//         //       .attr('x', (d, i) => {
+//         //           var x0 = Math.floor(i / 100) % 5, x1 = Math.floor(i % 10);
+//         //           return groupSpacing * x0 + (cellSpacing + size) * (x1 + x0 * 10);
+//         //       })
+//         //       .attr('y', (d, i) => {
+//         //           var y0 = Math.floor(i / 500), y1 = Math.floor(i % 100 / 10);
+//         //           return groupSpacing * y0 + (cellSpacing + size) * (y1 + y0 * 10);
+//         //       }) 
+//         //       .attr('width', size)
+//         //       .attr('height', size)
+//         //       .attr('fill', '#000000')
+//         //       .style('opacity',0)        
         
 
-var changeColor = function(data){
+// var changeColor = function(data){
 
-chocolate
-    .style('opacity',data)
-tree
-    .style('opacity',0)    
-pod
-    .style('opacity',0)    
+// chocolate
+//     .style('opacity',data)
+// // tree
+// //     .style('opacity',0)    
+// // pod
+// //     .style('opacity',0)    
 
-} 
+// } 
 
-
-var changeSize = function(data){
-    
-chocolate    
-    .style('opacity',0)    
-pod
-    .style('opacity',data)    
-tree
-    .style('opacity',0)    
-
-    
-}
-
-var changeShape = function(data){
-chocolate
-    .style('opacity',0)    
-
-pod
-    .style('opacity',0)    
-
-tree
-    .style('opacity',data)
-    
-}
-
-
-
-// })
