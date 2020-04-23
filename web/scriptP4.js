b7b0080964503a4f1377828b38a23eef1a50adec
EC2 Default User <ec2-user@ip-172-31-27-45.ec2.internal>
2020-04-23 23:14:44 +0000

    add graph in section 4

--- web/scriptP4.js
+++ web/scriptP4.js
@@ -46,13 +46,13 @@ var mouseover3 = function (d) {
     d3.select(this)
         .attr("fill", "tomato")        
         .style("font-weight",600)
-        .style("stroke-width", 2);
+        .style("stroke-width", 1);
 
 }
 
 var mouseleave3 = function (d) {
     d3.select(this)
-        .attr("fill", "#000000")        
+        .attr("fill", "#FFFAF0")        
         .style("font-weight",400)
         .style("stroke-width", 0);
 
@@ -289,17 +289,13 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .data(data)
         .enter()
         .append("rect")
-        .attr('x', function(d,i){
-            return width/5 + i*250
-        })
-        
         .attr("x",function(d,i) {
             if ( i<4) {
-         	  return width/4 + i*250;
+         	  return width/4 + i*250 + 50;
             } else if (i<8) {
-              return width/4 + (i-4)*250
+              return width/4 + (i-4)*250 + 50
             } else  {
-              return width/4 + (i-8)*250
+              return width/4 + (i-8)*250 + 50
             } 
     	})
         .attr("y",function(d,i) {
@@ -317,7 +313,7 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .attr('height', barHeight)
         .attr('stroke', '#FFFAF0')
         .attr('stroke-width', 0)
-        .attr('fill', '#000000')
+        .attr('fill', '#FFFAF0')
         .on("mouseover", mouseover3)
         .on("mouseleave", mouseleave3)
         
@@ -328,17 +324,13 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .data(data)
         .enter()
         .append("rect")
-        .attr('x', function(d,i){
-            return width/5 + i*250
-        })
-        
         .attr("x",function(d,i) {
             if ( i<4) {
-         	  return width/4 + i*250+barMargin;
+         	  return width/4 + i*250 + barMargin + 50;
             } else if (i<8) {
-              return width/4 + (i-4)*250+barMargin;
+              return width/4 + (i-4)*250+barMargin + 50;
             } else  {
-              return width/4 + (i-8)*250+barMargin;
+              return width/4 + (i-8)*250+barMargin + 50;
             } 
     	})
         .attr("y",function(d,i) {
@@ -356,7 +348,7 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .attr('height', barHeight)
         .attr('stroke', '#FFFAF0')
         .attr('stroke-width', 0)
-        .attr('fill', '#000000')       
+        .attr('fill', '#FFFAF0')       
         .on("mouseover", mouseover3)
         .on("mouseleave", mouseleave3)
 
@@ -367,17 +359,13 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .data(data)
         .enter()
         .append("rect")
-        .attr('x', function(d,i){
-            return width/5 + i*250
-        })
-        
         .attr("x",function(d,i) {
             if ( i<4) {
-         	  return width/4 + i*250+barMargin*2;
+         	  return width/4 + i*250+barMargin*2 + 50;
             } else if (i<8) {
-              return width/4 + (i-4)*250+barMargin*2;
+              return width/4 + (i-4)*250+barMargin*2 + 50;
             } else  {
-              return width/4 + (i-8)*250+barMargin*2;
+              return width/4 + (i-8)*250+barMargin*2 + 50;
             } 
     	})
         .attr("y",function(d,i) {
@@ -395,7 +383,7 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .attr('height', barHeight)
         .attr('stroke', '#FFFAF0')
         .attr('stroke-width', 0)
-        .attr('fill', '#000000')        
+        .attr('fill', '#FFFAF0')        
         .on("mouseover", mouseover3)
         .on("mouseleave", mouseleave3)
 
@@ -406,17 +394,13 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .data(data)
         .enter()
         .append("rect")
-        .attr('x', function(d,i){
-            return width/5 + i*250
-        })
-        
         .attr("x",function(d,i) {
             if ( i<4) {
-         	  return width/4 + i*250+barMargin*3;
+         	  return width/4 + i*250+barMargin*3 + 50;
             } else if (i<8) {
-              return width/4 + (i-4)*250+barMargin*3;
+              return width/4 + (i-4)*250+barMargin*3 + 50;
             } else  {
-              return width/4 + (i-8)*250+barMargin*3;
+              return width/4 + (i-8)*250+barMargin*3 + 50;
             } 
     	})
         .attr("y",function(d,i) {
@@ -434,7 +418,7 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .attr('height', barHeight)
         .attr('stroke', '#FFFAF0')
         .attr('stroke-width', 0)
-        .attr('fill', '#000000')   
+        .attr('fill', '#FFFAF0')   
         .on("mouseover", mouseover3)
         .on("mouseleave", mouseleave3)
  
@@ -444,17 +428,13 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .data(data)
         .enter()
         .append("rect")
-        .attr('x', function(d,i){
-            return width/5 + i*250
-        })
-        
         .attr("x",function(d,i) {
             if ( i<4) {
-         	  return width/4 + i*250+barMargin*4;
+         	  return width/4 + i*250+barMargin*4 + 50;
             } else if (i<8) {
-              return width/4 + (i-4)*250+barMargin*4;
+              return width/4 + (i-4)*250+barMargin*4 + 50;
             } else  {
-              return width/4 + (i-8)*250+barMargin*4;
+              return width/4 + (i-8)*250+barMargin*4 + 50;
             } 
     	})
         .attr("y",function(d,i) {
@@ -472,7 +452,7 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .attr('height', barHeight)
         .attr('stroke', '#FFFAF0')
         .attr('stroke-width', 0)
-        .attr('fill', '#000000')    
+        .attr('fill', '#FFFAF0')    
         .on("mouseover", mouseover3)
         .on("mouseleave", mouseleave3)
     
@@ -482,17 +462,13 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .data(data)
         .enter()
         .append("rect")
-        .attr('x', function(d,i){
-            return width/5 + i*250
-        })
-        
         .attr("x",function(d,i) {
             if ( i<4) {
-         	  return width/4 + i*250+barMargin*5;
+         	  return width/4 + i*250+barMargin*5 + 50;
             } else if (i<8) {
-              return width/4 + (i-4)*250+barMargin*5;
+              return width/4 + (i-4)*250+barMargin*5 + 50;
             } else  {
-              return width/4 + (i-8)*250+barMargin*5;
+              return width/4 + (i-8)*250+barMargin*5 + 50;
             } 
     	})
         .attr("y",function(d,i) {
@@ -510,12 +486,139 @@ d3.csv("./data/companiesCommitment.csv").then(function (data) {
         .attr('height', barHeight)
         .attr('stroke', '#FFFAF0')
         .attr('stroke-width', 0)
-        .attr('fill', '#000000')       
+        .attr('fill', '#FFFAF0')       
         .on("mouseover", mouseover3)
         .on("mouseleave", mouseleave3)   
-    
-    
-    
+ 
+//All Names
+    svg3.append("g")
+        .selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("x",function(d,i) {
+            if ( i<4) {
+         	  return width/4 + i*250 + 50;
+            } else if (i<8) {
+              return width/4 + (i-4)*250 + 50
+            } else  {
+              return width/4 + (i-8)*250 + 50
+            } 
+    	})
+        .attr("y",function(d,i) {
+            if ( i<4) {
+         	  return 100+barHeight;
+            } else if (i<8) {
+              return 300+barHeight;
+            } else  {
+              return 500+barHeight;
+            } 
+    	})
+        .style("fill","#FFFAF0")
+        .style("text-anchor", "start")
+        .style("font-size", "12px")
+        .style("font-family", "gopher")
+        .text(function(d) {
+        	return d.company;
+        })
+        .attr("opacity",1)
+
+//Left Legend
+for(i=0;i<6;i++){
+    svg3
+        .append("rect")
+        .attr("x",i*22 + 50)
+        .attr("y",80)
+        .attr('width', 20)
+        .attr('height', barHeight)
+        .attr('stroke', '#FFFAF0')
+        .attr('stroke-width', 0)
+        .attr('fill', '#FFFAF0')
+        .on("mouseover", mouseover3)
+        .on("mouseleave", mouseleave3)
+}   
+
+var seq= [1,2,3,4,5,6]
+var seq2= [4,2,1]
+
+//Legends---All Numbers
+    svg3.append("g")
+        .selectAll("text")
+        .data(seq)
+        .enter()
+        .append("text")
+        .attr("x",function(d,i) {
+            return i*22 + 57;
+        })
+        .attr("y", 40+barHeight)
+        .style("fill","#000000")
+        .style("text-anchor", "start")
+        .style("font-size", "12px")
+        .style("font-family", "courier")
+        .text(function(d,i) {
+        	return d;
+        })
+        .attr("opacity",1)
 
 
+//Legends---All Numbers
+    svg3.append("text")
+        .attr("x",50)
+        .attr("y", 100+barHeight)
+        .style("fill","FFFAF0")
+        .style("text-anchor", "start")
+        .style("font-size", "10px")
+        .style("font-family", "courier")
+        .text("1.Improving West African Cocoa")
+        .attr("opacity",1)
+
+    svg3.append("text")
+        .attr("x",50)
+        .attr("y", 100+barHeight+15)
+        .style("fill","FFFAF0")
+        .style("text-anchor", "start")
+        .style("font-size", "10px")
+        .style("font-family", "courier")
+        .text("2.Agroforestry")
+        .attr("opacity",1)
+
+    svg3.append("text")
+        .attr("x",50)
+        .attr("y", 100+barHeight+15*2)
+        .style("fill","FFFAF0")
+        .style("text-anchor", "start")
+        .style("font-size", "10px")
+        .style("font-family", "courier")
+        .text("3.Deforestation Free")
+        .attr("opacity",1)
+
+    svg3.append("text")
+        .attr("x",50)
+        .attr("y", 100+barHeight+15*3)
+        .style("fill","FFFAF0")
+        .style("text-anchor", "start")
+        .style("font-size", "10px")
+        .style("font-family", "courier")
+        .text("4.Save Water Usage")
+        .attr("opacity",1)
+
+    svg3.append("text")
+        .attr("x",50)
+        .attr("y", 100+barHeight+15*4)
+        .style("fill","FFFAF0")
+        .style("text-anchor", "start")
+        .style("font-size", "10px")
+        .style("font-family", "courier")
+        .text("5.Traceability of Cacao")
+        .attr("opacity",1)
+
+    svg3.append("text")
+        .attr("x",50)
+        .attr("y", 100+barHeight+15*5)
+        .style("fill","FFFAF0")
+        .style("text-anchor", "start")
+        .style("font-size", "10px")
+        .style("font-family", "courier")
+        .text("6.Improve Living Income")
+        .attr("opacity",1)
 })
